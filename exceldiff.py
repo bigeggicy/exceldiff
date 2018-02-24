0313b1b3a1b13c126e89a960d6048b69ec2ad02f
bigegg <lijinfeng@bupt.edu.cn>
2018-02-24 16:12:20 +0800

    修改row_compare规则，两行全空视为空，有不空的行的空视为不同

--- exceldiff.py
+++ exceldiff.py
@@ -60,13 +60,12 @@ def cell_compare(cell1, cell2):
 # 行列的函数比较，行/列所有单元格不同才返回不同
 def row_compare(row1, row2):
     n = min(len(row1), len(row2))
-    for i in range(0, n):
-        if (row1[i].value == ""and row2[i].value =="") is False and row1[i].value == row2[i].value:
-            return True  # 只要有一个相同，返回相同，视为相同行/列
     all_empty_flap = 1
-    for i in range(0,n):
-        if (row1[i].value == ""and row2[i].value =="") is False:
+    for i in range(0, n):
+        if (row1[i].value == ""and row2[i].value == "") is False:
             all_empty_flap = 0
+            if row1[i].value == row2[i].value:
+                return True  # 只要有一个相同，返回相同，视为相同行/列
     if all_empty_flap == 1:
         return True
     return False
